31cda71defc4d8995c8b537032e8d06750584215
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-06-15 14:48:51 -0300

    Learn DLL System.Collections.Generic

---
+++ mslearn-dotnet-files/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Collections.Generic;
+
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);     
+var salesFiles = FindFiles(storesDirectory);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
+
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//     foreach (var file in foundFiles)
+//     {
+//         // The file name will contain the full path, so only check the end of it
+//         if (file.EndsWith("sales.json"))
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
+
+//     return salesFiles;
+// }
+
+
+
+//var salesFiles = FindFiles("stores");
+//foreach (var file in salesFiles)
+//{
+//    Console.WriteLine(file);
+//}
