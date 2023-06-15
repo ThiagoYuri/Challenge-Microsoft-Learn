d77bc39724605ce7a30ac30cb85624930cbc349b
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-06-15 09:27:49 -0300

    ConsoleInVisualCode

---
+++ ConsoleInVisualCode/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World1!");
+int teste = 4;
+
+Console.WriteLine("Hello, World2!");
+Console.WriteLine("Hello, World3!");
+Console.WriteLine("Hello, World4!");
+Console.WriteLine("Hello, World5!");
+
+
